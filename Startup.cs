fd2a8f1a7a2ade815b9297ccd4e5067ad11336a2
Haley <hksmith2@buffs.wtamu.edu>
2020-02-02 13:30:54 -0600

    second commit

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -24,12 +26,16 @@ namespace MvcMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+        services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
